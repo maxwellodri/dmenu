75ccf4f1fdf2ce3b5e09a0cac9ba8852bc6c1d5f
Maxwell Odri <maxwellodri@gmail.com>
2024-09-20 12:42:28 +1000

    match hl colour to dwm (purplepink)

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#4c004c" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
